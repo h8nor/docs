f50889ff5cb854ec5a34d2b770698042315d96a6
bopoh13 <bopoh13@ya.ru>
2016-07-13 12:43:51 +0300

    Firefox 47 hardening
    This is a user.js configuration file for Mozilla Firefox that's supposed
    to harden Firefox's settings and make it more secure

---
+++ user.js
@@ -0,0 +1,207 @@
+# Mozilla FireFox 47 Secure Settings
+# Some settings http://warfx.ru/firefox/config/ have been abolished
+# Drop this file in a folder XXXXXXXX.default at %APPDATA%\Mozilla\Firefox\Profiles\
+
+# CRITICAL
+// Disable heartbeat
+// https://wiki.mozilla.org/Advocacy/heartbeat
+user_pref("browser.selfsupport.url", "");
+// Disable sending of the health report
+// https://support.mozilla.org/en-US/kb/firefox-health-report-understand-your-browser-perf
+user_pref("datareporting.healthreport.uploadEnabled", false);
+// Opt-out of add-on metadata updates
+// https://blog.mozilla.org/addons/how-to-opt-out-of-add-on-metadata-updates/
+user_pref("extensions.getAddons.cache.enabled", false);
+// https://gecko.readthedocs.org/en/latest/toolkit/components/telemetry/telemetry/preferences.html
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.server", "");
+user_pref("toolkit.telemetry.cachedClientID", "");
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("dom.ipc.plugins.flash.subprocess.crashreporter.enabled", false);
+user_pref("dom.ipc.plugins.reportCrashURL", false);
+user_pref("browser.tabs.crashReporting.sendReport", false);
+user_pref("security.ssl.errorReporting.enabled", false);
+// https://wiki.mozilla.org/Telemetry/Experiments
+user_pref("experiments.enabled", false);
+user_pref("experiments.manifest.uri", "");
+user_pref("experiments.supported", false);
+user_pref("experiments.activeExperiment", false);
+user_pref("network.allow-experiments", false);
+// Disable Location-Aware Browsing
+// http://www.mozilla.org/en-US/firefox/geolocation/
+user_pref("geo.enabled", false);
+user_pref("geo.wifi.uri", "");
+// https://support.mozilla.org/en-US/kb/how-stop-firefox-making-automatic-connections#w_geolocation-for-default-search-engine
+user_pref("browser.search.geoip.url", "");
+// http://kb.mozillazine.org/Network.proxy.type
+// the default in Firefox for Linux is to use system proxy settings.
+// We change it to direct connection
+user_pref("network.proxy.type", 0);
+// Don't reveal your internal IP
+// Check the settings with: http://net.ipcalf.com/
+// https://wiki.mozilla.org/Media/WebRTC/Privacy
+user_pref("media.peerconnection.enabled", false);
+user_pref("media.peerconnection.video.enabled", false);
+user_pref("media.peerconnection.turn.disable", true);
+user_pref("media.peerconnection.use_document_iceservers", false);
+user_pref("media.peerconnection.identity.timeout", 1);
+// Disable notifications
+user_pref("dom.webnotifications.enabled", false);
+user_pref("dom.webnotifications.serviceworker.enabled", false);
+user_pref("dom.serviceWorkers.enabled", false);
+user_pref("dom.push.serverURL", "");
+user_pref("dom.push.enabled", false);
+user_pref("dom.push.connection.enabled", false);
+// Disable scripts to manipulate browser windows
+user_pref("dom.disable_beforeunload", true);
+user_pref("dom.disable_window_move_resize", true);
+user_pref("dom.disable_window_open_feature.close", true);
+user_pref("dom.disable_window_open_feature.minimizable", true);
+user_pref("dom.disable_window_open_feature.personalbar", true);
+user_pref("dom.disable_window_open_feature.titlebar", true);
+// https://developer.mozilla.org/en-US/docs/Web/API/navigator.sendBeacon
+user_pref("beacon.enabled", false);
+// Disable pocket
+// https://support.mozilla.org/en-US/kb/save-web-pages-later-pocket-firefox
+user_pref("extensions.pocket.api", "");
+user_pref("extensions.pocket.enabled", false);
+user_pref("extensions.pocket.oAuthConsumerKey", "");
+user_pref("extensions.pocket.site", "");
+// Disable firefox hello
+// https://wiki.mozilla.org/Loop
+user_pref("loop.enabled", false);
+user_pref("loop.server", "");
+// Show "http(s)://" in the URL bar
+user_pref("browser.urlbar.trimURLs", false);
+// Disable new tab tile ads & preload
+user_pref("browser.newtab.preload", false);
+// https://wiki.mozilla.org/Tiles/Technical_Documentation#Ping
+// https://gecko.readthedocs.org/en/latest/browser/browser/DirectoryLinksProvider.html#browser-newtabpage-directory-ping
+user_pref("browser.newtabpage.directory.ping", "");
+// https://gecko.readthedocs.org/en/latest/browser/browser/DirectoryLinksProvider.html#browser-newtabpage-directory-source
+user_pref("browser.newtabpage.directory.source", "");
+// https://wiki.mozilla.org/Privacy/Reviews/New_Tab
+user_pref("browser.newtabpage.enabled", false);
+// http://www.thewindowsclub.com/disable-remove-ad-tiles-from-firefox
+// http://forums.mozillazine.org/viewtopic.php?p=13876331#p13876331
+user_pref("browser.newtabpage.enhanced", false);
+// https://support.mozilla.org/en-US/kb/how-stop-firefox-making-automatic-connections#w_mozilla-content
+user_pref("browser.aboutHomeSnippets.updateUrl", "");
+// Disabling the social services that are integrated into the browser
+user_pref("social.toast-notifications.enabled", false);
+user_pref("social.whitelist", "");
+user_pref("social.directories", "");
+user_pref("social.remote-install.enabled", false);
+user_pref("social.share.activationPanelEnabled", false);
+user_pref("social.shareDirectory", "");
+// Disable link prefetching
+// https://wiki.mozilla.org/Privacy/Reviews/Necko
+user_pref("network.predictor.enabled", false);
+// http://kb.mozillazine.org/Network.prefetch-next
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Link_prefetching_FAQ#Is_there_a_preference_to_disable_link_prefetching.3F
+user_pref("network.prefetch-next", false);
+// http://kb.mozillazine.org/Network.dns.disablePrefetch
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Controlling_DNS_prefetching
+user_pref("network.dns.disablePrefetch", true);
+// https://support.mozilla.org/en-US/kb/how-stop-firefox-making-automatic-connections#w_speculative-pre-connections
+// https://bugzil.la/814169
+user_pref("network.http.speculative-parallel-limit", 0);
+// Display an error message indicating the entered information is not a valid
+// URL instead of asking from google.
+// http://kb.mozillazine.org/Keyword.enabled#Caveats
+user_pref("keyword.enabled", false);
+// Don't try to guess where i'm trying to go!!! e.g.: "http://foo" -> "http://(prefix)foo(suffix)"
+// http://www-archive.mozilla.org/docs/end-user/domain-guessing.html
+user_pref("browser.fixup.alternate.enabled", false);
+// Disabling plug-in Adobe Primetime Content Decryption Module (DRM), 
+// which allows to keep track of your browser
+user_pref("browser.eme.ui.enabled", false);
+user_pref("media.eme.apiVisible", false);
+user_pref("media.eme.enabled", false);
+user_pref("media.gmp-eme-adobe.enabled", false);
+// getUserMedia
+// https://wiki.mozilla.org/Media/getUserMedia
+// https://developer.mozilla.org/en-US/docs/Web/API/Navigator
+user_pref("media.navigator.enabled", false);
+// Disable getUserMedia screen sharing
+// https://mozilla.github.io/webrtc-landing/gum_test.html
+user_pref("media.getusermedia.screensharing.allowed_domains", "");
+user_pref("media.getusermedia.screensharing.enabled", false);
+// https://bugzil.la/654550
+user_pref("media.video_stats.enabled", false);
+// Disable face detection by default
+user_pref("camera.control.face_detection.enabled", false);
+
+# DESIRABLE
+// https://wiki.mozilla.org/Security:Renegotiation#security.ssl.treat_unsafe_negotiation_as_broken
+// see also CVE-2009-3555
+user_pref("security.ssl.treat_unsafe_negotiation_as_broken", true);
+user_pref("security.ssl3.dhe_rsa_aes_128_sha", false);
+user_pref("security.ssl3.dhe_rsa_aes_256_sha", false);
+user_pref("security.ssl3.ecdhe_ecdsa_rc4_128_sha", false);
+user_pref("security.ssl3.ecdhe_rsa_rc4_128_sha", false);
+user_pref("security.ssl3.rsa_rc4_128_md5", false);
+user_pref("security.ssl3.rsa_rc4_128_sha", false);
+// The number stored in the memory of page to go back (reduce memory consumption)
+user_pref("browser.sessionhistory.max_entries", 12);
+// https://developer.mozilla.org/en-US/docs/Mozilla/Preferences/Preference_reference/dom.event.clipboardevents.enabled
+user_pref("dom.event.clipboardevents.enabled", false);
+// Webpages will not be able to affect the right-click menu
+//user_pref("dom.event.contextmenu.enabled", false);
+// http://kb.mozillazine.org/Dom.storage.enabled
+// http://dev.w3.org/html5/webstorage/#dom-localstorage
+// you can also see this with Panopticlick's "DOM localStorage"
+user_pref("dom.storage.enabled", false);
+// Disable webGL
+// http://www.contextis.com/resources/blog/webgl-new-dimension-browser-exploitation/
+user_pref("webgl.disable-extensions", true);
+user_pref("webgl.disabled", true);
+user_pref("webgl.min_capability_mode", true);
+// https://wiki.mozilla.org/Security/Reviews/Firefox/NavigationTimingAPI
+user_pref("dom.enable_performance", false);
+user_pref("dom.enable_resource_timing", false);
+user_pref("dom.enable_user_timing", false);
+user_pref("dom.idle-observers-api.enabled", false);
+// http://asmjs.org/
+// https://www.mozilla.org/en-US/security/advisories/mfsa2015-29/
+// https://www.mozilla.org/en-US/security/advisories/mfsa2015-50/
+// https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2015-2712
+user_pref("javascript.options.asmjs", false);
+// https://developer.mozilla.org/en-US/docs/Web/API/BatteryManager
+user_pref("dom.battery.enabled", false);
+// Disable gamepad input
+// http://www.w3.org/TR/gamepad/
+user_pref("dom.gamepad.enabled", false);
+// Disable virtual reality devices
+// https://developer.mozilla.org/en-US/Firefox/Releases/36#Interfaces.2FAPIs.2FDOM
+user_pref("dom.vibrator.enabled", false);
+user_pref("dom.vr.oculus.enabled", false);
+user_pref("dom.vr.oculus050.enabled", false);
+// Disable sensor API
+// https://wiki.mozilla.org/Sensor_API
+user_pref("device.sensors.enabled", false);
+// WebIDE
+// https://trac.torproject.org/projects/tor/ticket/16222
+user_pref("devtools.webide.autoinstallADBHelper", false);
+user_pref("devtools.webide.autoinstallFxdtAdapters", false);
+user_pref("devtools.webide.enabled", false);
+// Do not add downloaded files to the list of "Recent Documents" (Windows)
+user_pref("browser.download.manager.addToRecentDocs", false);
+// Open results in a new tab, rather than the current
+user_pref("browser.search.openintab", true);
+// Remove the line "visit/search" in the drop-down menu when typing in the URL bar
+user_pref("browser.urlbar.unifiedcomplete", false);
+// Check the spelling in all text fields (and not only in the Textarea)
+user_pref("layout.spellcheckDefault", 2);
+
+# Settings for Adblock Plus
+user_pref("extensions.adblockplus.hideContributeButton", true);
+user_pref("extensions.adblockplus.notificationurl", "");
+user_pref("extensions.adblockplus.report_submiturl", "");
+user_pref("extensions.adblockplus.savestats", false);
+user_pref("extensions.adblockplus.subscriptions_antiadblockurl", "");
+user_pref("extensions.adblockplus.subscriptions_exceptionsurl", "");
+user_pref("extensions.adblockplus.subscriptions_fallbackurl", "");
+user_pref("extensions.adblockplus.subscriptions_listurl", "");
+user_pref("extensions.adblockplus.suppress_first_run_page", true);
