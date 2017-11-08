ba11db206190dc9ef8337792bd607c45e6eccecf
bopoh13 <bopoh13@ya.ru>
2017-11-08 21:05:44 +0300

    Update user script Cut out PR-people; test to edited 'contentText'

--- vendor/firefox/habrusers_blocker_gm.user.js
+++ vendor/firefox/habrusers_blocker_gm.user.js
@@ -8,11 +8,13 @@
 // @downloadURL	https://github.com/bopoh13/docs/raw/master/vendor/firefox/habrusers_blocker_gm.user.js
 // @homepageURL	https://github.com/bopoh13/docs/tree/master/vendor/firefox
 // @supportURL	https://github.com/bopoh13/docs/issues
-// @version	1.0.10
+// @version	1.1.0
 // @icon	https://geektimes.ru/images/favicons/apple-touch-icon-57x57.png
 // @include	https://geektimes.ru/*
 // @include	https://habrahabr.ru/*
 // @exclude	https://habrahabr.ru/company/*
+// @exclude	https://geektimes.ru/users/*
+// @exclude	https://habrahabr.ru/users/*
 // @grant	none
 // ==/UserScript==
 /**
@@ -52,12 +54,19 @@ var fablers = [
   'SmirkinDA',
   'tm',
   'YuliaSinyanskaya',
-]
+];
+var slob = [
+  'display_adv',
+  'sci-fi',
+];
 // Only for companies. Science fiction is hiding separately
 var pr = [
+  'alconost',
+  'blog_potok',
   'haysrussia',
   'hosting-cafe',
   'icanchoose',
+  'infobip',
   'JetBrains',
   'miip',
   'odnoklassniki',
@@ -92,25 +101,27 @@ var wampus = [
   'Idot',
   'ilyasok',
   'Ilyasyakubov',
+  'izhanov',
   'lakegull',
   'mistik_max',
   'Olga_Voronova',
-  'Samoglas',
   'Sormovich',
   'VenomBlood',
 ];
 $('.post__user-info').filter(function() {
-    return this.href.match(new RegExp('/' + fablers.join('|') + '/'));
+    return this.href.match(new RegExp('/' + fablers.join('|') + '/$'));
 }).parents('.post').css("background", "#ffe");
 $('.hub-link').filter(function() {
-    return this.href.match(new RegExp('/sci-fi/')); // Научная фантастика
+    return this.href.match(new RegExp('/' + slob.join('|') + '/$'));
 }).parents('.post').hide();
 $('.post__title_link').filter(function() {
-    return this.href.match(new RegExp('/company/' + pr.join('|') + '/'));
+    return this.href.match(new RegExp('/company/' + pr.join('|') + '/$'));
 }).parents('.post').hide();
 $('.post__title_link').filter(function() {
-    return this.href.match(new RegExp('/company/' + sellers.join('|') + '/'));
+    return this.href.match(new RegExp('/company/' + sellers.join('|') + '/$'));
 }).parents('.post').css("background", "#def");
 $('.user-info_inline.user-info').filter(function() {
-    return this.href.match(new RegExp('/' + wampus.join('|') + '/'));
+    return this.href.match(new RegExp('/' + wampus.join('|') + '/$'));
 }).parents('.comment').html('<div class="comment__message comment__message_banned">РЕДИСКА опубликовала эту надпись здесь</div>');
+console.log($('.is_blocked.for_users_only_msg').text().replace(new RegExp('[^\\s\\w,А-яЁё]'), '$& Впрочем, можно <a href="https://geektimes.ru/info/help/registration/#invite">откупиться</a>.'));
+$('.is_blocked.for_users_only_msg').html().replace(new RegExp('[^\\s\\w,А-яЁё]'), '$& Впрочем, можно <a href="https://geektimes.ru/info/help/registration/#invite">откупиться</a>.');
